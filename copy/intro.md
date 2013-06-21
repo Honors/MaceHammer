400f955cc7d8eba51b3dfae724fabe76e613b59d
Matt Neary <neary.matt@gmail.com>
2013-06-21 03:32:46 -0400

    Simple CMS added

---
+++ copy/intro.md
@@ -0,0 +1,22 @@
+Spherical Shape
+===============
+The "MaceHammer"  with it's spherical head and variety of interchangeable grip positions, is an evolution of the traditional sledgehammer and sledgehammer swing, while incorporating the planes of motion found in the exercises of the kettlebell and heavy rope.
+
+The spherical head provides for a greater striking area thereby diffusing the force of impact. Its shape also allows for  a "hit anywhere" surface, eliminating miss hits [off square] thus avoiding torquing through the hands, wrist and elbows.
+
+
+Longer Handle
+=============
+The longer handle of the MaceHammer creates a larger arc of motion for a better, more strenuous workout.
+
+
+Variety of Handles
+==================
+The unique handle options (patent pending) are interchangeable, [hands separated grip - 90, 60, 45 degrees] and the standard (180 degree) hands aligned grip, recruit and engage muscle groups to perform through multiple planes of motion. The hands-separated grip is similar to the kettlebell swing, with the addition of eccentric muscle contraction (muscle fibers lengthen) with the  active downward swing.
+
+These design features adhere to the theory that muscle contraction, both concentric and eccentric in various planes of motion, promotes improved muscle development and joint strength.
+
+
+Variable Weight
+===============
+The weight of the "MaceHammer" is adaptable to the type of workout [strength vs. conditioning] and the user's ability. This is accomplished by filling the spherical head with sand, lead-shot, or a mixture, with weights ranging from 8-40lbs.
--- server.js
+++ server.js
@@ -1,15 +1,82 @@
 var http = require('http'),
 	fs = require('fs');
+var Showdown = require('showdown');
+var converter = new Showdown.converter();
 	
+var parseTemplate = function(template, pattern, parser, cb) {
+	var embeds = [], embedReads = {};
+	template.replace(pattern, function(match, name) {
+		embeds.push(name);
+	});
+	
+	var substituteEmbeds = function() {
+		cb(template.replace(pattern, function(match, name) {
+			return parser(embedReads[name]);
+		}));
+	};
+	
+	embeds.forEach(function(embed) {
+		fs.readFile(__dirname + '/copy/' + embed, function(err, data) {
+			embedReads[embed] = ""+data;
+			if( Object.keys(embedReads).length == embeds.length ) {
+				substituteEmbeds();
+			}
+		});
+	});
+};	
 exports.module = http.createServer(function(req, res) {	
+	if( req.url.match(/^\/upload/) ) {
+		var buffer = [], file = req.url.substr(1).split('/')[1];
+		req.on("data", function(chunk) {buffer.push(chunk)});
+		req.on("end", function() {
+			fs.writeFile(__dirname + '/copy/' + file, buffer, function(err) {
+				res.end(JSON.stringify({ error: err }));
+			});
+		});		
+		return;
+	}
+
 	var path = __dirname + (req.url == '/' ? '/index.html' : req.url);
 	fs.stat(path, function(err, stat) {
-	    if (!err) {
+	    if (!err && req.url != '/') {
 			res.writeHead(200, {'Content-Type': path.match(/js$/)?'text/javascript':'text/html'});
 			fs.createReadStream(path).pipe(res);
-	    }else {
+	    } else if( req.url == '/' ) {
+	    	res.writeHead(200, {'Content-Type': path.match(/js$/)?'text/javascript':'text/html'});
+	    	fs.readFile(__dirname + '/tmpl.html', function(err, data) {
+	    		var template = ""+data;
+	    		var parse_embeds = function(cb, template) {
+		    		parseTemplate(template, /@embed{([^}]+)}/g, function(md) {
+		    			return converter.makeHtml(md);
+		    		}, function(composite) {
+		    			cb(composite);
+		    		});
+				};
+	    		var parse_jsons = function(cb, template) {
+	    			parseTemplate(template, /@json{([^}]+)}/g, function(md) {
+	    				var html = converter.makeHtml(md);
+	    				var sections = html.split('<h1').map(function(section) {
+	    					return '<h1'+section.replace(/\n/g, '');
+	    				}).slice(1);
+	    				var dict = {};
+	    				sections.forEach(function(section) {
+	    					var key = section.match(/^<h1[^>]*?>([\s\S]+)<\/h1>/)[1];
+	    					var value = section.replace(/^<h1[^>]*?>[\s\S]+<\/h1>/, '');
+	    					dict[key] = value;
+	    				});
+	    				return JSON.stringify(dict);
+	    			}, function(composite) {
+	    				cb(composite);
+	    			});		
+	    		};	 
+	    		
+	    		parse_embeds(parse_jsons.bind({}, function(html) {
+	    			res.end(html);
+	    		}), template);
+	    	});
+	    } else {
 	        res.writeHead(404);
 	        res.end();
 	    }
 	});
-});
+}).listen(8080);

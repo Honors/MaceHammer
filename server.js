9f3ed0c3dece89346b8c5d85ef313240df16c8d6
Matt Neary <neary.matt@gmail.com>
2013-06-22 01:22:56 -0400

    storere page, fully editable

--- server.js
+++ server.js
@@ -24,6 +24,56 @@ var parseTemplate = function(template, pattern, parser, cb) {
 		});
 	});
 };	
+var renderTemplate = function(template, cb) {
+	var parse_embeds = function(cb, template) {
+		if( !template.match(/@embed{([^}]+)}/g) ) {
+			cb(template);
+		}
+		parseTemplate(template, /@embed{([^}]+)}/g, function(md) {
+			return converter.makeHtml(md).replace(/\n/g, ' ');
+		}, function(composite) {
+			cb(composite);
+		});
+	};
+	var parse_raws = function(cb, template) {
+		if( !template.match(/@raw{([^}]+)}/g) ) {
+			cb(template);
+		}
+		parseTemplate(template, /@raw{([^}]+)}/g, function(json) {
+			return json;
+		}, function(composite) {
+			cb(composite);
+		});
+	};
+	var parse_jsons = function(cb, template) {
+		if( !template.match(/@json{([^}]+)}/g) ) {
+			cb(template);
+		}
+		parseTemplate(template, /@json{([^}]+)}/g, function(md) {
+			var html = converter.makeHtml(md);
+			var sections = html.split('<h1').map(function(section) {
+				return '<h1'+section.replace(/\n/g, '');
+			}).slice(1);
+			var dict = {};
+			sections.forEach(function(section) {
+				var key = section.match(/^<h1[^>]*?>([\s\S]+)<\/h1>/)[1];
+				var value = section.replace(/^<h1[^>]*?>[\s\S]+<\/h1>/, '');
+				dict[key] = value;
+			});
+			return JSON.stringify(dict);
+		}, function(composite) {
+			cb(composite);
+		});		
+	};	 	    	
+	
+	parse_embeds(parse_jsons.bind({}, parse_raws.bind({}, cb)), template);
+};
+var findProduct = function(SKU, products) {
+	for( var k in products ) {
+		if( products[k].SKU == SKU ) return products[k];
+	}
+	return {};
+};
 exports.module = http.createServer(function(req, res) {	
 	if( req.url.match(/^\/upload/) ) {
 		var buffer = [], file = req.url.substr(1).split('/')[1];
@@ -31,6 +81,22 @@ exports.module = http.createServer(function(req, res) {
 		req.on("end", function() {
 			fs.writeFile(__dirname + '/copy/' + file, buffer, function(err) {
 				res.end(JSON.stringify({ error: err }));
+			});
+		});		
+		return;
+	} else if( req.url == '/pay' ) {
+		var buffer = [], file = req.url.substr(1).split('/')[1];
+		req.on("data", function(chunk) {buffer.push(chunk)});
+		req.on("end", function() {
+			var purchase = JSON.parse(buffer.join(""));
+			fs.readFile(__dirname + '/copy/products.json', function(err, data) {
+				var products = JSON.parse(""+data),
+					product = findProduct(purchase.SKU, products),
+					token = purchase.token;
+				
+				// TODO: process payment with stripe...
+				
+				res.end(JSON.stringify({ err: null, success: true }));
 			});
 		});		
 		return;
@@ -45,38 +111,21 @@ exports.module = http.createServer(function(req, res) {
 	    	res.writeHead(200, {'Content-Type': path.match(/js$/)?'text/javascript':'text/html'});
 	    	fs.readFile(__dirname + '/tmpl.html', function(err, data) {
 	    		var template = ""+data;
-	    		var parse_embeds = function(cb, template) {
-		    		parseTemplate(template, /@embed{([^}]+)}/g, function(md) {
-		    			return converter.makeHtml(md);
-		    		}, function(composite) {
-		    			cb(composite);
-		    		});
-				};
-	    		var parse_jsons = function(cb, template) {
-	    			parseTemplate(template, /@json{([^}]+)}/g, function(md) {
-	    				var html = converter.makeHtml(md);
-	    				var sections = html.split('<h1').map(function(section) {
-	    					return '<h1'+section.replace(/\n/g, '');
-	    				}).slice(1);
-	    				var dict = {};
-	    				sections.forEach(function(section) {
-	    					var key = section.match(/^<h1[^>]*?>([\s\S]+)<\/h1>/)[1];
-	    					var value = section.replace(/^<h1[^>]*?>[\s\S]+<\/h1>/, '');
-	    					dict[key] = value;
-	    				});
-	    				return JSON.stringify(dict);
-	    			}, function(composite) {
-	    				cb(composite);
-	    			});		
-	    		};	 
-	    		
-	    		parse_embeds(parse_jsons.bind({}, function(html) {
+	    		renderTemplate(template, function(html) {
 	    			res.end(html);
-	    		}), template);
+	    		});
+	    	});
+	    } else if( req.url == '/store' ) {
+	    	res.writeHead(200, {'Content-Type': path.match(/js$/)?'text/javascript':'text/html'});
+	    	fs.readFile(__dirname + '/store.html', function(err, data) {
+	    		var template = ""+data;
+	    		renderTemplate(template, function(html) {
+	    			res.end(html);
+	    		});
 	    	});
 	    } else {
 	        res.writeHead(404);
 	        res.end();
 	    }
 	});
-});
+}).listen(8080);
